8fb7e0628733aa8a3409c521d3124f3ad97b73f8
Roja-ucla <roja@ucla.edu>
2013-02-10 20:35:00 -0800

    fixed a bug (accessing V(Gi) before being assigned)

--- code_v3_streamlined/vertexwrite.R
+++ code_v3_streamlined/vertexwrite.R
@@ -3,7 +3,7 @@ write(c("com","vertices","edges","density"),file="communityStats",ncolumns=4)
 for (i in 0:(length(memberships$csize)-1)){
 	Vi=which(memberships$membership==i)-1 # V is the index of nodes in V(G) that belong to community i
 	if (length(Vi)<4){
-		write(c(i,length(V(Gi)),"NA","NA"),file="communityStats",ncolumns=4,append=TRUE)
+		write(c(i,length(Vi),"NA","NA"),file="communityStats",ncolumns=4,append=TRUE)
 		next}
 	Gi=subgraph(G,Vi) # V(Gi) will have the "names" of the nodes in V(G) that belong to community i
 	filename=paste('community',i,sep="")
